2f53eb742a00bcf3239a3806fe1c41267a4e6460
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2021-12-07 21:44:23 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
 control.onEvent(EventBusSource.MICROBIT_ID_IO_P0, EventBusValue.MICROBIT_PIN_EVT_PULSE_HI, function () {
-	
+    Fente += 1
+    Distance.push(Fente / 4 * 9.57)
+    Temps.push(input.runningTimeMicros() / 1000000)
+    serial.writeString("" + Temps[list.length] + "," + Distance[list.length] + "," + (Distance[Distance.length] - Distance[Distance.length - 1]) / (Temps[Temps.length] - Temps[Temps.length - 1]))
 })
+let Temps: number[] = []
+let Distance: number[] = []
+let list: number[][] = []
+let Fente = 0
 basic.showIcon(IconNames.Yes)
 radio.setGroup(1)
 pins.setEvents(DigitalPin.P0, PinEventType.Pulse)
-let Fente = 0
-let Distance = 0
+Fente = 0
+list = [Distance, Temps]
