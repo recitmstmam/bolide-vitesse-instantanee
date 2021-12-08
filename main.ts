de90430f7ac95ed5389f3096af98240a839be2bf
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2021-12-07 23:00:18 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,31 @@
 control.onEvent(EventBusSource.MICROBIT_ID_IO_P0, EventBusValue.MICROBIT_PIN_EVT_PULSE_HI, function () {
+    let Distance: number[] = []
+    let Temps: number[] = []
     Fente += 1
-    Distance.push(Fente / 4 * 9.57)
-    Temps.push(input.runningTimeMicros() / 1000000)
-    serial.writeString("" + Temps[list.length] + "," + Distance[list.length] + "," + (Distance[list.length] - Distance[list.length - 1]) / (Temps[list.length] - Temps[list.length - 1]))
+    Temps[0] = input.runningTimeMicros() / 1000000
+    t1 = Temps.removeAt(0)
+    Distance[1] = Fente / 4 * 9.57 / 100
+    d1 = Distance.removeAt(1)
+    Vitesse = (d2 - d1) / (t2 - t1)
+    serial.writeString("" + (Vitesse))
+    serial.writeLine("")
+    d2 = d1
+    t2 = t1
 })
-let Temps: number[] = []
-let Distance: number[] = []
-let list: number[][] = []
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Diamond)
+    basic.pause(500)
+    basic.showString("!")
+    control.reset()
+})
+let t2 = 0
+let d2 = 0
+let Vitesse = 0
+let d1 = 0
+let t1 = 0
 let Fente = 0
 basic.showIcon(IconNames.Yes)
 radio.setGroup(1)
 pins.setEvents(DigitalPin.P0, PinEventType.Pulse)
 Fente = 0
-list = [Distance, Temps]
+let list = [0, 1]
