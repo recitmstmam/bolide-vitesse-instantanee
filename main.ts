c3dce8946996b61bcc2e8f2f978364be22096f88
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2021-12-07 23:47:54 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,11 @@ control.onEvent(EventBusSource.MICROBIT_ID_IO_P0, EventBusValue.MICROBIT_PIN_EVT
     Fente += 1
     t1 = input.runningTimeMicros() / 1000000
     d1 = Fente / 4 * 9.57 / 100
-    Vitesse = Math.abs(d3 - d1) / Math.abs(t3 - t1)
-    radio.sendString("" + d1 + "," + t1 + "," + d2 + "," + t2 + "," + d3 + "," + t3 + "," + Vitesse)
+    Vitesse = Math.abs(d2 - d1) / Math.abs(t2 - t1)
+    serial.writeString("" + d1 + "," + t1 + "," + d2 + "," + t2 + "," + Vitesse)
+    serial.writeLine("")
     t2 = t1
     d2 = d1
-    t3 = t2
-    d3 = d2
 })
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Diamond)
@@ -15,12 +14,10 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("!")
     control.reset()
 })
-let Vitesse = 0
-let d3 = 0
-let d2 = 0
-let d1 = 0
-let t3 = 0
 let t2 = 0
+let d2 = 0
+let Vitesse = 0
+let d1 = 0
 let t1 = 0
 let Fente = 0
 basic.showIcon(IconNames.Yes)
@@ -28,8 +25,4 @@ radio.setGroup(1)
 pins.setEvents(DigitalPin.P0, PinEventType.Pulse)
 Fente = 0
 t1 = 0
-t2 = 0
-t3 = 0
 d1 = 0
-d2 = 0
-d3 = 0
