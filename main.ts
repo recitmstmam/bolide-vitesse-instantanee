160375c4c265466fbc78c6224209e9e3dad93b00
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2022-02-08 09:33:14 -0500

    Restaurer '16/12/2021, 15:35:23 Mise à jour main.blocks'

--- main.ts
+++ main.ts
@@ -1,22 +1,5 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Diamond)
-    basic.pause(500)
-    basic.showIcon(IconNames.SmallDiamond)
-    control.reset()
-})
-pins.onPulsed(DigitalPin.P0, PulseValue.Low, function () {
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P0, EventBusValue.MICROBIT_PIN_EVT_PULSE_HI, function () {
     Fente += 1
-})
-let t2 = 0
-let d2 = 0
-let Vitesse = 0
-basic.showIcon(IconNames.Yes)
-radio.setGroup(1)
-pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
-let Fente = 0
-let t1 = 0
-let d1 = 0
-basic.forever(function () {
     t1 = input.runningTimeMicros() / 1000000
     d1 = Fente / 6 * 9.6 / 100
     Vitesse = Math.abs(d2 - d1) / Math.abs(t2 - t1)
@@ -24,3 +7,21 @@ basic.forever(function () {
     t2 = t1
     d2 = d1
 })
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Diamond)
+    basic.pause(500)
+    basic.showString("!")
+    control.reset()
+})
+let t2 = 0
+let d2 = 0
+let Vitesse = 0
+let d1 = 0
+let t1 = 0
+let Fente = 0
+basic.showIcon(IconNames.Yes)
+radio.setGroup(1)
+pins.setEvents(DigitalPin.P0, PinEventType.Pulse)
+Fente = 0
+t1 = 0
+d1 = 0
